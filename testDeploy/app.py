29ed6253671c0043cdfc3a9d9b498c02b7fde46f
hungsvdut2k2 <hungsvdut2k2@gmail.com>
2022-09-17 19:36:42 +0700

    add UI of detection website

---
+++ testDeploy/app.py
@@ -0,0 +1,15 @@
+from distutils.log import debug
+from flask import Flask, render_template
+import torch
+from PIL import Image
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
